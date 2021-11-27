4d83b451adabe982c716ad602fcb648f365a6e4f
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-27 20:58:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+
+import { Container } from "@mui/material";
+
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import ContactsPage from "./pages/ContactsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+import MyAppBar from "./components/MyAppBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MyAppBar />
+
+      <Container style={{ maxWidth: "1650px", paddingBottom: "40px" }}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+
+          <Route path="/register" component={RegisterPage} />
+
+          <Route path="/login" component={LoginPage} />
+
+          <Route path="/contacts" component={ContactsPage} />
+
+          <Route>
+            <NotFoundPage />
+          </Route>
+        </Switch>
+      </Container>
+    </>
   );
 }
 
