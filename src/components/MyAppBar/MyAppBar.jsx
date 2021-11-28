9810e95b941c8f6dba3a7b24f54a56118a1a4960
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-28 03:44:29 +0100

    Update MyAppBar.jsx

--- src/components/MyAppBar/MyAppBar.jsx
+++ src/components/MyAppBar/MyAppBar.jsx
@@ -1,14 +1,23 @@
+import { useSelector } from "react-redux";
+
 import Navigation from "../Navigation";
 import UserMenu from "../UserMenu";
 import AuthNav from "../AuthNav";
 import Header from "../Header";
 
+import { authSelectors } from "../../redux/auth";
+
 import { ContactPhone } from "@mui/icons-material";
 import { Box, Toolbar, AppBar, Container, Icon } from "@mui/material";
 
 export default function MyAppBar() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
   return (
-    <AppBar position="static">
+    <AppBar
+      position="static"
+      sx={{ paddingTop: "10px", paddingBottom: "10px" }}
+    >
       <Toolbar>
         <Icon color="inherit" sx={{ width: "60px", height: "60px" }}>
           <ContactPhone sx={{ width: "100%", height: "100%" }} />
@@ -28,13 +37,15 @@ export default function MyAppBar() {
             <Navigation />
           </Box>
 
-          <Box>
-            <AuthNav />
-          </Box>
-
-          <Box>
-            <UserMenu />
-          </Box>
+          {isLoggedIn ? (
+            <Box>
+              <UserMenu />
+            </Box>
+          ) : (
+            <Box>
+              <AuthNav />
+            </Box>
+          )}
         </Container>
       </Toolbar>
     </AppBar>
