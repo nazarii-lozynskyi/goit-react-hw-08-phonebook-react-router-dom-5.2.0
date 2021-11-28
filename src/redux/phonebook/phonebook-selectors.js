091a3f2cc4a0326f7c204fb137a15ce3aba08b80
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-28 14:36:47 +0100

    Update phonebook-selectors.js

--- src/redux/phonebook/phonebook-selectors.js
+++ src/redux/phonebook/phonebook-selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 const getLoading = (state) => state.contacts.loading;
 const getFilter = (state) => state.contacts.filter;
 const getAllContacts = (state) => state.contacts.items;
@@ -7,10 +9,21 @@ const getTotalContactsCount = (state) => {
   return contacts.length;
 };
 
+const getVisibleContacts = createSelector(
+  [getAllContacts, getFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ description }) =>
+      description.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
+
 const contactsSelectors = {
   getLoading,
   getFilter,
   getAllContacts,
   getTotalContactsCount,
+  getVisibleContacts,
 };
 export default contactsSelectors;
