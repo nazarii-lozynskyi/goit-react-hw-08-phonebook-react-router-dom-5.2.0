ea7afabb7f5871f624daebc5da506cf11bf6edd9
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-28 17:34:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,59 +1,67 @@
-import { Route, Switch } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import { useEffect, Suspense, lazy } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { Container } from "@mui/material";
-
-import HomePage from "./pages/HomePage";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
-import ContactsPage from "./pages/ContactsPage";
-import NotFoundPage from "./pages/NotFoundPage";
 
 import MyAppBar from "./components/MyAppBar";
 
 import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
 
-import { authOperations } from "./redux/auth";
+import { authOperations, authSelectors } from "./redux/auth";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const ContactsPage = lazy(() => import("./pages/ContactsPage"));
+//const UploadView = lazy(() => import("./views/UploadView"));
 
 function App() {
   const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
   }, [dispatch]);
 
   return (
-    <>
-      <MyAppBar />
+    !isFetchingCurrentUser && (
+      <>
+        <MyAppBar />
 
-      <Container style={{ maxWidth: "1650px", paddingBottom: "40px" }}>
-        <Switch>
-          <Suspense fallback={<p>Загружаем...</p>}>
-            <PublicRoute exact path="/">
-              <HomePage />
-            </PublicRoute>
+        <Container style={{ maxWidth: "1650px", paddingBottom: "40px" }}>
+          <Switch>
+            <Suspense fallback={<p>Загружаем...</p>}>
+              <PublicRoute exact path="/">
+                <HomePage />
+              </PublicRoute>
 
-            <PublicRoute exact path="/register" restricted>
-              <RegisterPage />
-            </PublicRoute>
+              <PublicRoute exact path="/register" restricted>
+                <RegisterPage />
+              </PublicRoute>
 
-            <PublicRoute exact path="/login" restricted redirectTo="/contacts">
-              <LoginPage />
-            </PublicRoute>
+              <PublicRoute
+                exact
+                path="/login"
+                restricted
+                redirectTo="/contacts"
+              >
+                <LoginPage />
+              </PublicRoute>
 
-            <PrivateRoute path="/contacts" redirectTo="/login">
-              <ContactsPage />
-            </PrivateRoute>
+              <PrivateRoute path="/contacts" redirectTo="/login">
+                <ContactsPage />
+              </PrivateRoute>
 
-            {/*<Route>
+              {/*<Route>
               <NotFoundPage />
             </Route>*/}
-          </Suspense>
-        </Switch>
-      </Container>
-    </>
+            </Suspense>
+          </Switch>
+        </Container>
+      </>
+    )
   );
 }
 
