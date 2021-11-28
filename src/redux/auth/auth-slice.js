497ef3ec24c58f08affc0e5577557ce3d4347976
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-28 03:44:49 +0100

    Update auth-slice.js

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -11,43 +11,34 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  extraReducers: {},
+  extraReducers: {
+    [authOperations.register.fulfilled](state, action) {
+      state.user = action.payload.user.name;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+    [authOperations.logIn.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+    [authOperations.logOut.fulfilled](state) {
+      state.user = { name: null, email: null };
+      state.token = null;
+      state.isLoggedIn = false;
+    },
+    //    [authOperations.fetchCurrentUser.pending](state) {
+    //      state.isFetchingCurrentUser = true;
+    //    },
+    //    [authOperations.fetchCurrentUser.fulfilled](state, action) {
+    //      state.user = action.payload;
+    //      state.isLoggedIn = true;
+    //      state.isFetchingCurrentUser = false;
+    //    },
+    //    [authOperations.fetchCurrentUser.rejected](state) {
+    //      state.isFetchingCurrentUser = false;
+    //},
+  },
 });
-
-//const authSlice = createSlice({
-//  name: 'auth',
-//  initialState,
-//  extraReducers: {
-//    [authOperations.register.fulfilled](state, action) {
-//      state.user = {
-//        name: action.payload.user.name,
-//        email: action.payload.user.email,
-//      };
-//      state.token = action.payload.token;
-//      state.isLoggedIn = true;
-//    },
-//    [authOperations.logIn.fulfilled](state, action) {
-//      state.user = action.payload.user;
-//      state.token = action.payload.token;
-//      state.isLoggedIn = true;
-//    },
-//    [authOperations.logOut.fulfilled](state) {
-//      state.user = { name: null, email: null };
-//      state.token = null;
-//      state.isLoggedIn = false;
-//    },
-//    [authOperations.fetchCurrentUser.pending](state) {
-//      state.isFetchingCurrentUser = true;
-//    },
-//    [authOperations.fetchCurrentUser.fulfilled](state, action) {
-//      state.user = action.payload;
-//      state.isLoggedIn = true;
-//      state.isFetchingCurrentUser = false;
-//    },
-//    [authOperations.fetchCurrentUser.rejected](state) {
-//      state.isFetchingCurrentUser = false;
-//    },
-//  },
-//});
 
 export default authSlice.reducer;
